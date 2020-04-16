8b0c674247e863b58a133938674acefc9ed9e5a5
malcolm-mclean <mclean.malcolm.a@gmail.com>
2020-04-16 17:02:01 -0600

    Add routes for examples with links

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+const UseStatePage = lazy(() =>
+	import(
+		/* webpackChunkName: "useState" */ '../examples/useState/UseStatePage'
+	)
+);
+
+const UseContextPage = lazy(() =>
+	import(
+		/* webpackChunkName: "useContext" */ '../examples/useContext/UseContextPage'
+	)
+);
 
 const App = () => {
-	return <h1 data-test="app">This is just the beginning</h1>;
+	return (
+		<main>
+			<BrowserRouter>
+				<Suspense fallback={<p>Loading</p>}>
+					<Switch>
+						<Route path="/" exact>
+							<ul>
+								<li>
+									<Link to="/usestate">
+										useState Examples
+									</Link>
+								</li>
+								<li>
+									<Link to="/usecontext">
+										useContext Examples
+									</Link>
+								</li>
+							</ul>
+						</Route>
+						<Route path="/usestate" exact>
+							<UseStatePage />
+						</Route>
+						<Route path="/usecontext" exact>
+							<UseContextPage />
+						</Route>
+					</Switch>
+				</Suspense>
+			</BrowserRouter>
+		</main>
+	);
 };
 
 export default App;
